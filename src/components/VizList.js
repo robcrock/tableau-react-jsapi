66c2a65fc1b96d14e72874130e224961887275ac
Robert Crocker <35023759+robcrock@users.noreply.github.com>
2020-11-12 06:35:50 -0300

    Update VizList.js
    Add my own URLs

--- src/components/VizList.js
+++ src/components/VizList.js
@@ -1,9 +1,16 @@
 const dashboards = [
-  "https://public.tableau.com/views/RegionalSampleWorkbook/Flights",
-  "https://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
-  "https://public.tableau.com/views/RegionalSampleWorkbook/College",
-  "https://public.tableau.com/views/RegionalSampleWorkbook/Stocks",
-  "https://public.tableau.com/views/RegionalSampleWorkbook/Storms"
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi1",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi2",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi3",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi4",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi5",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi6",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi7",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi8",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi9",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi10",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi11",
+  "https://public.tableau.com/views/SocialMediaKPIs_16051099238290/kpi12"
 ];
 
 export default dashboards;
